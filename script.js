62f52a78927a44313ca0ed9e7a73adfb8b7456cd
Gaurav Kumar <53636036+gk9848970@users.noreply.github.com>
2021-08-13 23:53:02 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+//  Mobile Nav
+
 const hamBurger = document.querySelector('.hamburger__icon')
 const mobileNav = document.querySelector('.mobile__ul')
 
@@ -6,19 +8,36 @@ hamBurger.addEventListener('click', () => {
     mobileNav.classList.toggle('active')
 })
 
+document.body.addEventListener('click', (e)=> {
+    if(e.target.closest('.mobile__ul') || e.target.matches('.hamburger__icon')) return;
+
+    mobileNav.classList.remove('active')
+})
+
+// Faq Box
+
 const questionWrapper = document.querySelector('.faq__questionWrapper')
 
 questionWrapper.addEventListener('click', (e) => {
     if(!e.target.closest('.faq__box')) return;
 
     const faqBox = e.target.closest('.faq__box');
-    faqBox.classList.toggle('active')
+    const faqContent = faqBox.querySelector('.faq__content')
+    faqContent.classList.toggle('active')
 })
 
-document.body.addEventListener('click', (e)=> {
-    console.log("Hi");
-    if(e.target.closest('.mobile__ul') || e.target.matches('.hamburger__icon')) return;
+// Modal
 
-    console.log("Hi");
-    mobileNav.classList.remove('active')
+const modalBtn = document.querySelector('.modal-btn');
+const modalBtnClose = document.querySelector('.modal-close-btn');
+const modal = document.querySelector('.modal');
+
+modalBtn.addEventListener("click", () => {
+  setTimeout(()=> {
+    modal.classList.add('open');
+  }, 100)
+})
+
+modalBtnClose.addEventListener("click", () => {
+    modal.classList.remove('open');
 })
